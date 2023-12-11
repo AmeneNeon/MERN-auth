f96fc734c8822dcc611449a8820f84a8a9214884
AmeneNeon <ameneneon@gamil.com>
2023-12-11 14:58:03 +0200

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,12 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+  console.log('MongoDB connected successfully!'); 
+}).catch((err)=>{
+  console.error('MongoDB connection error:', err)
+})
+
 
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+
+app.listen(3000, () => {
+  console.log(`Server is running on port: 3000`);
 });
