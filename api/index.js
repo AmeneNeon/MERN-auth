a3fc857e277a81bf9f293eee3d3feae8c170e9dd
AmeneNeon <ameneneon@gamil.com>
2023-12-11 15:21:43 +0200

    create user model

--- api/index.js
+++ api/index.js
@@ -1,18 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
-  console.log('MongoDB connected successfully!'); 
-}).catch((err)=>{
-  console.error('MongoDB connection error:', err)
-})
-
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB connected successfully!");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
 
 const app = express();
-
-
 
 app.listen(3000, () => {
   console.log(`Server is running on port: 3000`);
