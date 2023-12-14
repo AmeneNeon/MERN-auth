1f5f5c4a376921bb4986ce404ed3f15fd6dfd4fd
AmeneNeon <ameneneon@gamil.com>
2023-12-14 14:01:22 +0200

    add middleware and a function to handel errors

--- api/index.js
+++ api/index.js
@@ -1,10 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoute from './routes/user.route.js';
-import authRoute from './routes/auth.route.js'
-
-
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -18,13 +16,23 @@ mongoose
   });
 
 const app = express();
-app.use(express.json())
-
-app.use('/api/user',userRoute)
-
-app.use('/api/auth',authRoute)
-
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log(`Server is running on port: 3000`);
 });
+
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+
+app.use((err,req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || " Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message: message,
+    statusCode: statusCode,
+  });
+});
+
+
