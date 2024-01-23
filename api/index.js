0778008ef19d7c914c582f2d3dcf3ea8245b9883
AmeneNeon <ameneneon@gamil.com>
2024-01-23 09:44:27 +0200

    create update user api route

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 import authGoogle from "./routes/auth.route.js"
-
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -20,6 +20,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.listen(3000, () => {
   console.log(`Server is running on port: 3000`);
